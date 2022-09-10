f0d9a7d4f38e45f17fbd5542ab6af79b314de233
maverickJAI <jaimech61@gmail.com>
2022-09-10 15:23:25 +0530

    updated actiopn todo

--- src/action/todo.js
+++ src/action/todo.js
@@ -1,2 +1,16 @@
-import React, { Component } from 'react';
+
 import {ADD_TODO, REMOVE_TODO} from "./action-types"
+
+// action carring which action and what action to perfrom
+export const addTodo = (todo) => ({
+
+    type:ADD_TODO,
+    payload:todo
+})
+
+
+export const removeTodo = (id) => ({
+
+    type:REMOVE_TODO,
+    payload:id
+})
